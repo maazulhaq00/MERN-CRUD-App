08b89ba2d9506c801b0a71378eca739b54da2b09
maaz <maazulhaq00@gmail.com>
2024-11-14 12:57:58 +0500

    React creat component & consuming express POST API, & DELETE API

--- frontend/src/pages/CreateStudentPage.js
+++ frontend/src/pages/CreateStudentPage.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function CreateStudentPage() {
+
+  const navigate = useNavigate();
+
+  const [student, setStudent] = useState({
+    name: '',
+    age: 0,
+    email: ''
+  })
+
+  const handleInputChange = (e) => {
+
+    let { name, value } = e.target;
+
+    setStudent({
+      ...student,
+      [name]: value
+    })
+
+    // console.log(student);
+  }
+
+  const handleFormSubmit = async (e) => {
+    
+    e.preventDefault();
+
+    const res = await axios.post("http://localhost:3000/students", student);
+
+    navigate('/students');
+
+  }
+
+
+
   return (
-    <div>CreateStudentPage</div>
+    <>
+      <h1>Create Student</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Name: <input type='text' name='name' onChange={handleInputChange} value={student.name} />
+        </label><br />
+        <label>
+          Age: <input type='number' name='age' onChange={handleInputChange} value={student.age} />
+        </label><br />
+        <label>
+          Email: <input type='email' name='email' onChange={handleInputChange} value={student.email} />
+        </label><br />
+        <input type='submit' value='Create' />
+      </form>
+    </>
   )
 }
 
--- frontend/src/pages/StudentsPage.js
+++ frontend/src/pages/StudentsPage.js
@@ -5,48 +5,65 @@ import axios from "axios";
 
 function StudentsPage() {
 
-    const [students, setStudents] = useState(null);
+  const [students, setStudents] = useState(null);
 
-    const fetchStudents = async () => {
-  
-      // fetch students from api
-      const res = await axios.get("http://localhost:3000/students");
-      console.log(res.data.students);
-  
-  
-      // set students in state
-      setStudents(res.data.students);
-    }
-  
-    useEffect(() => {
-      fetchStudents()
-    }, []);
-  
-  
-    return (
-      <>
-        <h1>Students</h1>
-  
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Email</th>
-          </tr>
-          {
-            students != null ?
-              students.map((student) => {
-                return <tr>
-                  <td>{student.name}</td>
-                  <td>{student.age}</td>
-                  <td>{student.email}</td>
-                </tr>
-              }) : <h3>No Record</h3>
-          }
-        </table>
-  
-      </>
-    );
+  const fetchStudents = async () => {
+
+    // fetch students from api
+    const res = await axios.get("http://localhost:3000/students");
+    console.log(res.data.students);
+
+
+    // set students in state
+    setStudents(res.data.students);
+  }
+
+  useEffect(() => {
+    fetchStudents()
+  }, []);
+
+  const deleteStudent = async (_id) => {
+    // console.log(_id);
+    const res = await axios.delete(`http://localhost:3000/students/${_id}`);
+
+    fetchStudents();
+
+  }
+
+  const editStudent = (_id) => {
+
+  }
+
+
+  return (
+    <>
+      <h1>Students</h1>
+
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Email</th>
+          <th>Action</th>
+        </tr>
+        {
+          students != null ?
+            students.map((student) => {
+              return <tr>
+                <td>{student.name}</td>
+                <td>{student.age}</td>
+                <td>{student.email}</td>
+                <td>
+                  <button onClick={() => editStudent(student._id)}>Edit</button>
+                  <button onClick={() => deleteStudent(student._id)}>Delete</button>
+                </td>
+              </tr>
+            }) : <h3>No Record</h3>
+        }
+      </table>
+
+    </>
+  );
 }
 
 export default StudentsPage
